586ad868f66913cb973cc3fbffd977c2fda4617e
KA PO <36360197+kapannn@users.noreply.github.com>
2020-01-27 08:54:26 -0800

    Add files via upload

---
+++ while_hmm.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+#ბლოკი #1
+
+a = ('Hmm2')
+print (str(a) + '\n\t(◔‿◔)｡◕‿◕｡')
+b = 'Hmm3'
+print (str(b) + '\n\t')
+c = 'hmm4'
+print (str(c) + '\n\t')
+d = 'Hmm5678910'
+print (str(d) + '\n\t')
+
+#ბლოკი #2
+
+while d == 'Hmm5678910':
+     print (d)
